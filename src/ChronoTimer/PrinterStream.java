290a6739bbf4637766542a93872385d8c32dfd6b
rpthorne <rpthorne@uwm.edu>
2017-04-15 12:04:26 -0500

    issues arise with CT displaying info from CT instead of just run.

---
+++ src/ChronoTimer/PrinterStream.java
@@ -0,0 +1,30 @@
+package ChronoTimer;
+
+import java.io.PrintStream;
+
+import javax.swing.JTextArea;
+
+public class PrinterStream extends PrintStream 
+{
+
+	JTextArea textArea;
+	
+	public PrinterStream(JTextArea outputArea) 
+	{
+		super(System.out);
+		textArea = outputArea;
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void println(String string) 
+	{
+		textArea.append(string+"\n");
+	}
+	 
+	public void print(String string) 
+	{
+		textArea.append(string);
+	}
+
+}
