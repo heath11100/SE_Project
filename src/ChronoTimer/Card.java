3f2cfc888bdd4aeefb6ea8df6abd40fb4fb091b6
heath11100 <heath11100@gmail.com>
2017-04-27 19:56:08 -0500

    Cleaned Card class for useless informaiton.

--- src/ChronoTimer/Card.java
+++ src/ChronoTimer/Card.java
@@ -1,8 +1,6 @@
 package ChronoTimer;
 import java.util.Queue;
 
-import Exceptions.InvalidTimeException;
-
 public class Card extends UIPrint {
 	private String header, body, footer;
 	
@@ -17,24 +15,19 @@ public class Card extends UIPrint {
 	public String writeTo()
 	{
 		refresh();
-		//this.setText(header + body + footer);
-		//System.out.println(header + body + footer);
-		return "RACE";
+		return "RUN";
 	}
 
 	public void setHeader(String string) {
 		header = string;
 		refresh();
-		//this.replaceRange(header, 0, headerSize-1);
 	}
 	
 	public void setHeader(Queue<Racer> newHeader) {
-		//don't care about times for queue, right?
 		header = "";
 		for (Racer r: newHeader)
 			header = r.toString() + "\n" + header;
 		refresh();
-		//this.replaceRange(header, 0, headerSize-1);
 	}
 
 	public void setBody(String newBody) {
@@ -42,27 +35,9 @@ public class Card extends UIPrint {
 		refresh();
 	}
 
-	public void setBody(Queue<Racer> newBody){
-		body = "";
-
-		for (Racer r : newBody) {
-			try {
-				ChronoTime elapsedTime = r.getElapsedTime();
-
-				body = r.toString() + elapsedTime + "\n" + body;
-			} catch (InvalidTimeException e) {
-				//Then the Racer will not be added.
-				//We should never reach this point anyways.
-			}
-		}
-		refresh();
-		//this.replaceRange(body, headerSize+1, headerSize+bodySize);
-	}
-
 	public void setFooter(String string) {
 		footer = string;
 		refresh();
-		//this.replaceRange(footer, headerSize+bodySize+2, headerSize+bodySize+footerSize+1);
 	}
 
 	private void refresh() {
@@ -72,12 +47,8 @@ public class Card extends UIPrint {
 	}
 
 	@Override
-	public void up() {
-		//until we decide to do something with these, these are really just for the menu class
-	}
+	public void up() { }
 
 	@Override
-	public void down() {
-		//until we decide to do something with these, these are really just for the menu class
-	}
+	public void down() { }
 }
