ae7b941067302433e47048b9516b13050ab87c1e
heath11100 <heath11100@gmail.com>
2017-05-03 19:02:53 -0500

    Added Test files for INDRun and ALL Managers (yet to be implemented).

---
+++ src/Tests/whiteBox/Run/Managers/TestAllManagers.java
@@ -0,0 +1,17 @@
+package Tests.whiteBox.Run.Managers;
+
+/**
+ * Created by austinheath on 5/3/17.
+ */
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestINDRunManager.class
+})
+
+public class TestAllManagers {
+}
---
+++ src/Tests/whiteBox/Run/Managers/TestINDRunManager.java
@@ -0,0 +1,17 @@
+package Tests.whiteBox.Run.Managers;
+
+/**
+ * Created by austinheath on 5/3/17.
+ */
+
+import ChronoTimer.Runs.INDRunManager;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        INDRunManager.TestINDRunManager.class
+})
+
+public class TestINDRunManager {
+}
